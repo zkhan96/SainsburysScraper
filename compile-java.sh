2f7ecd2837a6b6d0e56da029a0715de71d71d1c4
Zohaib <zohaib.m.khan96@gmail.com>
2018-04-25 07:11:49 +0100

    added creation of folders for those that are starting the project for the first time

--- compile-java.sh
+++ compile-java.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir out out/production out/production/SainsburysScraper
 javac -d out/production/SainsburysScraper src/*.java
 jar cfvm SainsburysScraper.jar src/META-INF/MANIFEST.MF -C out/production/SainsburysScraper/ SainsburysScraper.class
