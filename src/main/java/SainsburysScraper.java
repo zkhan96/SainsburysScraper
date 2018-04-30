bf3fe5d1efaac3b143aea98ba94bf79da38cc0d3
Zohaib Khan <zohaib.khan@thehutgroup.com>
2018-05-01 00:31:48 +0100

    better logging and looking at item descendants of my desired class

--- src/main/java/SainsburysScraper.java
+++ src/main/java/SainsburysScraper.java
@@ -1,19 +1,21 @@
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 public class SainsburysScraper {
 
   private final static String SAINSBURYS_URL = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
 
+  private final static Logger LOG = Logger.getGlobal();
   public static void main(String[] args) {
 
     Optional<HtmlPage> page = SimpleHtmlPageGetter.getPageForUrl(SAINSBURYS_URL);
-
     if (!page.isPresent()) {
-      System.err.println("The page returned by url: {} is invalid and therefore cannot be parsed.");
+      LOG.severe("Sainsbury's page cannot be parsed. ");
+      return;
     }
 
     HtmlPage htmlPage = page.get();
-    System.out.println(htmlPage.getTitleText());
+    htmlPage.getDocumentElement().getHtmlElementDescendants().forEach(System.out::println);
   }
 }
